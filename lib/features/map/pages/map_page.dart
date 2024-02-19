d7a61841e0bf225243865339e6b157781d33e11b
sukhrob-djumaev <sukhrob.djumaev@gmail.com>
2024-02-19 20:55:01 +0500

    change map to google maps

--- lib/features/map/pages/map_page.dart
+++ lib/features/map/pages/map_page.dart
@@ -1,9 +1,8 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_map/flutter_map.dart';
-import 'package:flutter_map_location_marker/flutter_map_location_marker.dart';
-import 'package:latlong2/latlong.dart';
-import 'package:street_kind/gen/assets.gen.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 @RoutePage()
 class MapPage extends StatelessWidget {
@@ -11,62 +10,47 @@ class MapPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return FlutterMap(
-      options: const MapOptions(
-        initialCenter: LatLng(41.3076182, 69.2627155),
-        initialZoom: 13,
+    return Scaffold(
+      body: SafeArea(
+        child: GoogleMap(
+            mapType: MapType.hybrid,
+            initialCameraPosition: const CameraPosition(
+              target: LatLng(41.3076182, 69.2627155),
+              zoom: 113,
+            ),
+
+            // myLocationEnabled: true,
+            myLocationButtonEnabled: true,
+            myLocationEnabled: true,
+            zoomControlsEnabled: true,
+            markers: {
+              const Marker(
+                markerId: MarkerId('1'),
+                position: LatLng(41.338752, 69.338418),
+              ),
+              const Marker(
+                markerId: MarkerId('2'),
+                position: LatLng(41.363771, 69.298103),
+              ),
+              const Marker(
+                markerId: MarkerId('3'),
+                position: LatLng(41.353627, 69.288752),
+              ),
+              const Marker(
+                markerId: MarkerId('4'),
+                position: LatLng(41.334101, 69.285653),
+              ),
+              const Marker(
+                markerId: MarkerId('5'),
+                position: LatLng(41.322992, 69.311880),
+              ),
+              const Marker(
+                markerId: MarkerId('6'),
+                position: LatLng(41.285884, 69.253321),
+              ),
+            },
+            gestureRecognizers: {}..add(Factory<EagerGestureRecognizer>(() => EagerGestureRecognizer()))),
       ),
-      children: [
-        TileLayer(
-          urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-          userAgentPackageName: 'com.example.app',
-        ),
-        CurrentLocationLayer(
-          alignPositionOnUpdate: AlignOnUpdate.always,
-        ),
-        MarkerLayer(
-          markers: [
-            Marker(
-              rotate: true,
-              point: const LatLng(41.338752, 69.338418),
-              child: Assets.images.raster.pawpointer.image(width: 80),
-            ),
-            Marker(
-              rotate: true,
-              point: const LatLng(41.363771, 69.298103),
-              child: Assets.images.raster.pawpointer.image(width: 80),
-            ),
-            Marker(
-              rotate: true,
-              point: const LatLng(41.353627, 69.288752),
-              child: Assets.images.raster.pawpointer.image(width: 80),
-            ),
-            Marker(
-              rotate: true,
-              point: const LatLng(41.334101, 69.285653),
-              child: Assets.images.raster.pawpointer.image(width: 80),
-            ),
-            Marker(
-              rotate: true,
-              point: const LatLng(41.322992, 69.311880),
-              child: Assets.images.raster.pawpointer.image(width: 80),
-            ),
-            Marker(
-              rotate: true,
-              point: const LatLng(41.285884, 69.253321),
-              child: Assets.images.raster.pawpointer.image(width: 80),
-            ),
-          ],
-        ),
-        // RichAttributionWidget(
-        //   attributions: [
-        //     TextSourceAttribution(
-        //       'OpenStreetMap contributors',
-        //       onTap: () => launchUrl(Uri.parse('https://openstreetmap.org/copyright')),
-        //     ),
-        //   ],
-        // ),
-      ],
     );
   }
 }
